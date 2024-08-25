6a8f455ecfb6e5e2dc96c47252cf8c9839a00c1a
Vikash Sharma <vs8824394430@gmail.com>
2024-08-26 00:03:03 +0530

    dom added

--- script.js
+++ script.js
@@ -4,3 +4,7 @@
 //onmouseout-->The user moves the mouse away from an HTML element
 //onkeydown-->The user pushes a keyboard key
 //onload-->The browser has finished loading the page
+
+//document.getElementById(id)-->find an element by element id
+//document.getElementsByTagName(name)-->Find elements by tag name
+//document.getElementsByClassName(name)	-->find element by class name
